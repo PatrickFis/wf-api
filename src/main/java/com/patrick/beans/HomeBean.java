d2a6ce833dd3839df0a2865930347b26b38d7628
PatrickFis <fischerpl@mail.lipscomb.edu>
2018-05-12 12:26:44 -0500

    Initial commit

---
+++ src/main/java/com/patrick/beans/HomeBean.java
@@ -0,0 +1,24 @@
+package com.patrick.beans;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class HomeBean implements Serializable {
+	private static final long serialVersionUID = 633040440398716820L;
+	
+	private String home = "Home page";
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+	
+	
+}

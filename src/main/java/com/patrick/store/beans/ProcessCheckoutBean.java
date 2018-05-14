ac7d34ed47fda95af9d77ee63be09c41f122adb0
PatrickFis <fischerpl@mail.lipscomb.edu>
2018-05-13 21:48:08 -0500

    More EE stuff, added code to 'process' purchases.

--- src/main/java/com/patrick/store/beans/ProcessCheckoutBean.java
+++ src/main/java/com/patrick/store/beans/ProcessCheckoutBean.java
@@ -2,6 +2,8 @@ package com.patrick.store.beans;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -14,9 +16,14 @@ import com.patrick.store.interfaces.Product;
 @ViewScoped
 public class ProcessCheckoutBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger("processCheckout");
 	
 	@Inject
 	private Cart cart;
+	
+	private List<Product> purchaseItems;
+	private String customerName;
+	private String customerAddress;
 	
 	public List<Product> getProductsInCart() {
 		return this.cart.getProductsInCart();
@@ -24,5 +31,33 @@ public class ProcessCheckoutBean implements Serializable {
 	
 	public double getTotalPrice() {
 		return this.cart.calculateTotalPrice();
+	}
+	
+	public List<Product> getPurchaseItems() {
+		return this.purchaseItems;
+	}
+	
+	public void setPurchaseItems(List<Product> purchaseItems) {
+		this.purchaseItems = purchaseItems;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	
+	public void submitPurchase() {
+		logger.log(Level.INFO, "Customer name {0}. Customer address {1}.", new Object[] {customerName, customerAddress});
 	}
 }

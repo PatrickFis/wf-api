e3845d39f77a764942431d545107222bd045e3c9
PatrickFis <fischerpl18@gmail.com>
2018-05-18 20:25:25 -0500

    Comment out main

--- src/main/java/com/patrick/store/noncdi/ProxyCaller.java
+++ src/main/java/com/patrick/store/noncdi/ProxyCaller.java
@@ -6,9 +6,9 @@ import java.lang.reflect.Proxy;
  * Demonstrates the problem with calling nested methods and handling them with a proxy.
  */
 public class ProxyCaller {
-	public static void main(String[] args) {
-		ProxyInterface proxyInterface = (ProxyInterface) Proxy.newProxyInstance(ProxyInterface.class.getClassLoader(), new Class<?>[] { ProxyInterface.class }, new MonitoredMethodInvocationHandler());
-		proxyInterface.parentMethod();
-		proxyInterface.subMethod();
-	}
+//	public static void main(String[] args) {
+//		ProxyInterface proxyInterface = (ProxyInterface) Proxy.newProxyInstance(ProxyInterface.class.getClassLoader(), new Class<?>[] { ProxyInterface.class }, new MonitoredMethodInvocationHandler());
+//		proxyInterface.parentMethod();
+//		proxyInterface.subMethod();
+//	}
 }
